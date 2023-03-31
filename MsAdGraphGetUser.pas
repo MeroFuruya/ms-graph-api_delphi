b70e25bd871931e546ec52c56d98ff88b376fe03
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2023-03-31 11:44:13 +0200

    added feature

--- MsAdGraphGetUser.pas
+++ MsAdGraphGetUser.pas
@@ -18,10 +18,13 @@ type
   private
     FHTTP: THTTPClient;
     FTenant: string;
+
+    function GetValue(AJ: TJsonValue; AKey: string; var AValue: string): boolean;
   public
     constructor Create(authenticator: TMsAdAuthenticator; tenantId: string);
     destructor Destroy; override;
     function GetUser(sysLogin: string): string;
+    function GetUsers: string;
   end;
 
 implementation
@@ -159,6 +162,178 @@ begin
         end;
       end;
       Aj.Free;
+    end;
+  end;
+end;
+
+function TMsAdGraph.GetUsers: string;
+var
+  ASelectFields: TArray<string>;
+  AQuery: string;
+  AUrl: string;
+  AReq: IHTTPRequest;
+  ARes: IHTTPResponse;
+  Aj: TJsonValue;
+  AJArr: TJSONArray;
+  AJUser: TJSONValue;
+  AJErr: TJsonValue;
+  AErr: TMsAdError;
+  AToken: string;
+
+  AJsonArrayList: TList<TJSONArray>;
+
+  AResCode: Integer;
+
+  displayName, givenName, surname, mail, bussinesPhone, mobilePhone, faxNumber: string;
+  sysLogin: string;
+begin
+  AToken := self.Token;
+
+  Result := '';
+
+  if AToken <> '' then
+  begin
+    ASelectFields := [
+      'businessPhones',
+      'displayName',
+      'faxNumber',
+      'givenName',
+      'mail',
+      'mobilePhone',
+      'surname',
+      'onPremisesSamAccountName'
+    ];
+    AQuery := Format(
+      '?$filter=accountEnabled eq true&$select=%s&$count=true',
+      [string.join(',', ASelectFields)]
+    );
+
+    //AQuery := '?$filter=onPremisesSamAccountName eq ''KEHL''';
+
+    AUrl := Format(
+      'https://graph.microsoft.com/v1.0/%s/users%s',
+      [self.FTenant, TNetEncoding.URL.EncodeQuery(AQuery)]
+    );
+
+    AResCode := 200;
+
+    AJsonArrayList := TList<TJSONArray>.Create;
+
+    while (AResCode = 200) and (AUrl <> '') do
+    begin
+      AReq := FHTTP.GetRequest(sHTTPMethodGet, AUrl);
+      AReq.Accept := 'application/json';
+
+      Areq.AddHeader('Authorization', AToken);
+      AReq.AddHeader('ConsistencyLevel', 'eventual');
+      ARes := self.FHTTP.Execute(AReq);
+
+      AResCode := ARes.StatusCode;
+
+      if AResCode <> 200 then
+      begin
+        AErr := Default(TMsAdError);
+        AErr.StatusCode := ARes.StatusCode;
+        AErr.StatusText := ARes.StatusText;
+        AErr.url := AUrl;
+        AErr.Method := sHTTPMethodGet;
+        AErr.req_Header := AReq.Headers;
+        AErr.res_header := ARes.Headers;
+        AErr.error_data := ARes.ContentAsString();
+
+        Aj := TJSONValue.ParseJSONValue(ARes.ContentAsString(TEncoding.UTF8));
+        if Aj.TryGetValue<TJsonValue>('error', AJErr) then
+        begin
+          AJErr.TryGetValue<string>('code', AErr.error_name);
+          AJErr.TryGetValue<string>('message', AErr.error_description);
+        end;
+        Aj.Free;
+        self.OnRequestError(AErr);
+      end
+      else
+      begin
+        // displayName, givenName, surname, mail, bussinesPhone, mobilePhone, faxNumber
+
+        Aj := TJSONValue.ParseJSONValue(ARes.ContentAsString(TEncoding.UTF8));
+        // get next url
+        if not self.GetValue(Aj, '@odata.nextLink', AUrl) then
+          AUrl := '';
+        
+        if Aj.TryGetValue<TJsonArray>('value', AJArr) then
+          AJsonArrayList.Add(TJSONArray(AJArr.Clone));
+        Aj.Free;
+      end;
+    end;
+    if AJsonArrayList.Count > 0 then
+    begin
+      Result := ''
+      + '<?xml version="1.0" standalone="yes"?>'
+      + '<DATAPACKET Version="2.0">'
+      + '<METADATA>'
+      + '<FIELDS>'
+      + '<FIELD attrname="SYSTEMLOGIN" fieldtype="string" WIDTH="60"/>'
+      + '<FIELD attrname="NAME" fieldtype="string" WIDTH="40"/>'
+      + '<FIELD attrname="EMAIL" fieldtype="string" WIDTH="255"/>'
+      + '<FIELD attrname="TELEFON" fieldtype="string" WIDTH="35"/>'
+      + '<FIELD attrname="VORNAME" fieldtype="string" WIDTH="35"/>'
+      + '<FIELD attrname="NACHNAME" fieldtype="string" WIDTH="35"/>'
+      + '<FIELD attrname="FAX" fieldtype="string" WIDTH="35"/>'
+      + '<FIELD attrname="MOBILTELEFON" fieldtype="string" WIDTH="35"/>'
+      + '</FIELDS>'
+      + '<PARAMS/>'
+      + '</METADATA>'
+      + '<ROWDATA>';
+      
+      for AJArr in AJsonArrayList do
+      begin
+        for AJUser in AJArr do
+        begin
+          AJUser.TryGetValue<string>('displayName', displayName);
+          AJUser.TryGetValue<string>('businessPhones[0]', bussinesPhone);
+          AJUser.TryGetValue<string>('givenName', givenName);
+          AJUser.TryGetValue<string>('surname', surname);
+          AJUser.TryGetValue<string>('mail', mail);
+          //AJUser.TryGetValue<string>('bussinesPhone', bussinesPhone);
+          AJUser.TryGetValue<string>('mobilePhone', mobilePhone);
+          AJUser.TryGetValue<string>('faxNumber', faxNumber);
+
+          Result := Result
+            + Format(''
+            + '<ROW SYSTEMLOGIN="%s" NAME="%s" EMAIL="%s" TELEFON="%s" '
+            + 'VORNAME="%s" NACHNAME="%s" FAX="%s" MOBILTELEFON="%s" />',
+            [sysLogin, displayName, mail, bussinesPhone, givenName, surname,
+              faxNumber, mobilePhone]
+          );
+        end;
+      end;
+      
+      Result := Result
+      + '</ROWDATA>'
+      + '</DATAPACKET>';
+    end
+    else
+    begin
+      Result := '';
+    end;
+  end;
+end;
+
+function TMsAdGraph.GetValue(AJ: TJsonValue; AKey: string; var AValue: string): boolean;
+var
+  AI: Integer;
+begin
+  Result := False;
+  AValue := '';
+  if AKey <> '' then
+  begin
+    for AI := 0 to TJSONObject(AJ).Count - 1 do
+    begin
+      if TJSONObject(AJ).Pairs[AI].JsonString.Value = AKey then
+      begin
+        AValue := TJSONObject(AJ).Pairs[AI].JsonValue.Value;
+        Result := True;
+        Break;
+      end;
     end;
   end;
 end;
--- Project1.dpr
+++ Project1.dpr
@@ -15,65 +15,113 @@ var
   graph: TMsAdGraph;
   stop: boolean;
   inp: string;
+
+  tenantId: string;
+  clientId: string;
+  redirectPort: string;
+  redirectPath: string;
+  redirectPort_int: integer;
+  redirectPort_word: word;
 begin
-  auth := TMsAdAuthenticator.Create(
-    TMsAdAuthenticator.TAthenticatorType.ATDelegated,
-    TMsAdClientInfo.Create(
-      '<tenantId>',
-      '<clientId>',
-      ['User.Read.All'],
-      TRedirectUri.Create(0000, '<redirectUri>'),
-      TMsAdTokenStorege.Create('LMPS')
-    ),
-    TMsAdClientEvents.Create(
-      procedure(ResponseInfo: THttpServerResponse)
-      begin
-        ResponseInfo.ContentStream := TStringStream.Create('<title>Login Succes</title>This Tab can be closed now :)');
-      end,
-      procedure(Error: TMsAderror)
-      begin
-        writeln(
-          Format(
-            ''
-            + '%sStatus: . . . . . %d : %s'
-            + '%sErrorName: . . .  %s'
-            + '%sErrorDescription: %s'
-            + '%sUrl: . . . . . .  %s %s'
-            + '%sData: . . . . . . %s',
-            [
-              sLineBreak, error.StatusCode, error.StatusText,
-              sLineBreak, error.error_name,
-              sLineBreak, error.error_description,
-              sLineBreak, error.Method, error.url,
-              sLineBreak, error.error_data
-            ]
-          )
-        );
-      end,
-      procedure(out Cancel: boolean)
-      begin
 
-      end
-    )
-  );
 
-  graph := TMsAdGraph.Create(auth, '<tenantId>');
+  tenantId := GetEnvironmentVariable('tenantId');
+  clientId := GetEnvironmentVariable('clientId');
+  redirectPort := GetEnvironmentVariable('redirectPort');
+  redirectPath := GetEnvironmentVariable('redirectPath');
 
-  stop := false;
-  while not stop do
+  if (tenantId = '') or (clientId = '') or (redirectPath = '') or (redirectPort = '') then
   begin
-    write('type sysLogin or quit: ');
-    ReadLn(inp);
-    if inp.ToLower = 'quit' then
-      stop := true
-    else
-    begin
-      Writeln('');
-      Writeln(graph.GetUser(inp));
-    end;
-  end;
+    Writeln('Please set the following environment variables:');
+    Writeln('tenantId');
+    Writeln('clientId');
+    Writeln('redirectPort');
+    Writeln('redirectPath');
+    Writeln('Press any key to exit..');
+    ReadLn;
+    Exit;
+  end
+  else if not TryStrToInt(redirectPort, redirectPort_int) then
+  begin
+    Writeln('redirectPort must be a number');
+    Writeln('Press any key to exit..');
+    ReadLn;
+    Exit;
+  end
+  else if (redirectPort_int < 0) or (redirectPort_int > 65535) then
+  begin
+    Writeln('redirectPort must be between 0 and 65535');
+    Writeln('Press any key to exit..');
+    ReadLn;
+    Exit;
+  end
+  else
+  begin
+    redirectPort_word := redirectPort_int;
+    auth := TMsAdAuthenticator.Create(
+      TMsAdAuthenticator.TAthenticatorType.ATDelegated,
+      TMsAdClientInfo.Create(
+        tenantId,
+        clientId,
+        ['User.Read.All'],
+        TRedirectUri.Create(redirectPort_word, redirectPath),
+        TMsAdTokenStorege.Create('LMPS')
+      ),
+      TMsAdClientEvents.Create(
+        procedure(ResponseInfo: THttpServerResponse)
+        begin
+          ResponseInfo.ContentStream := TStringStream.Create('<title>Login Succes</title>This Tab can be closed now :)');
+        end,
+        procedure(Error: TMsAderror)
+        begin
+          writeln(
+            Format(
+              ''
+              + '%sStatus: . . . . . %d : %s'
+              + '%sErrorName: . . .  %s'
+              + '%sErrorDescription: %s'
+              + '%sUrl: . . . . . .  %s %s'
+              + '%sData: . . . . . . %s',
+              [
+                sLineBreak, error.StatusCode, error.StatusText,
+                sLineBreak, error.error_name,
+                sLineBreak, error.error_description,
+                sLineBreak, error.Method, error.url,
+                sLineBreak, error.error_data
+              ]
+            )
+          );
+        end,
+        procedure(out Cancel: boolean)
+        begin
 
-  Writeln('Goodbye..');
-  graph.Free;
-  auth.Free;
+        end
+      )
+    );
+
+    graph := TMsAdGraph.Create(auth, tenantId);
+
+    stop := false;
+    while not stop do
+    begin
+      write('type sysLogin, all or quit: ');
+      ReadLn(inp);
+      if inp.ToLower = 'quit' then
+        stop := true
+      else if inp.ToLower = 'all' then
+      begin
+        Writeln('');
+        Writeln(graph.GetUsers);
+      end
+      else
+      begin
+        Writeln('');
+        Writeln(graph.GetUser(inp));
+      end;
+    end;
+
+    Writeln('Goodbye..');
+    graph.Free;
+    auth.Free;
+  end;
 end.
